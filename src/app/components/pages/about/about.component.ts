2217a23f5fa7460d6aab06622be31229be8dabb5
mamjerez <correomam@gmail.com>
2021-09-10 06:56:46 +0200

    Clean Code

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  templateUrl: './about.component.html'
 })
 export class AboutComponent { }
--- src/app/components/pages/faq/faq.component.ts
+++ src/app/components/pages/faq/faq.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
-  templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  templateUrl: './faq.component.html'
 })
-export class FaqComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FaqComponent { }
